e0825d9fdbd517b2502ced02ed88d434b981e207
IOSdeVeloper20 <81517945+IOSdeVeloper20@users.noreply.github.com>
2023-02-05 19:20:14 +0200

    add popular and special dishes

---
+++ FoodApp/Controller/DishDetailsVC.swift
@@ -0,0 +1,25 @@
+//
+//  DishDetailsVC.swift
+//  FoodApp
+//
+//  Created by ahmed sherif on 05/02/2023.
+//
+
+import UIKit
+
+class DishDetailsVC: UIViewController {
+    
+    @IBOutlet weak var DishDetailsImage: UIImageView!
+    @IBOutlet weak var DishDetailsTitleLabel: UILabel!
+    @IBOutlet weak var DishDetailsCaloriesLabel: UILabel!
+    @IBOutlet weak var DishDetailsDescreptionLabel: UILabel!
+    @IBOutlet weak var DishDetailsTextfield: UITextField!
+    @IBAction func DishDetailsButton(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
